c5e56411e5c1389db1b64d934801589d2abe1f2e
John Calvin Young <john@johncalvinyoung.com>
2013-01-14 21:31:38 -0500

    Initial commit of BornLoved.org project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bornloved::Application.config.secret_token = 'bfe584c286f528e4375c3a995012ffb3ffd20bf8b3161e6e54ae350d35d1cc0650b7d9cbc9d078aa0989b87769ce333e6b7c6f50210e4209c57af5f8508c0d8d'
